900c981fa6f6e097130baa34e5143c36a79ac1d8
smandyscom <smandyscom@gmail.com>
2018-07-04 23:47:43 +0800

    circle object build

---
+++ DrawObjects/CircleBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp2.Interface;
+
+namespace WindowsFormsApp2.DrawObjects
+{
+    public abstract class CircleBase : Idraw
+    {
+        protected static Pen __pen = new Pen(Color.Yellow, 3);
+
+        internal SnapPoint __center;
+        internal float __radius;
+
+
+        public CircleBase()
+        {
+            __center = new SnapPoint(this, PointType.center);
+        }
+
+        public bool isSelected
+        {
+            get;
+            set;
+        } = false;
+
+        public void draw(Graphics graphics)
+        {
+            graphics.DrawEllipse(__pen,__center.Location.X,__center.Location.Y,__radius/2,__radius/2);
+        }
+
+        public List<SnapPoint> GetSnapPoints()
+        {
+            return new List<SnapPoint>() {__center};
+        }
+
+        public bool isHitObject(PointF hit)
+        {
+            //throw new NotImplementedException();
+            return false;
+        }
+    }
+}

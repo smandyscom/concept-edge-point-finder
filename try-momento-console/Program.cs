12a3ccccb497ff1e54545124d8c6a758f6236ecb
smandyscom <smandyscom@gmail.com>
2019-01-08 23:10:10 +0800

    try momento pattern

---
+++ try-momento-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace try_momento_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

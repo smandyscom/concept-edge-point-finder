484c38dddf05ee2bf82c37409da2f575463b430c
smandyscom <smandyscom@gmail.com>
2019-01-30 07:36:12 -0800

    skatched find end points procedures

--- core/Derived/LineFitted.cs
+++ core/Derived/LineFitted.cs
@@ -40,7 +40,59 @@ namespace Core.Derived
                 Mat.Zeros(xVectors.Rows, 1, xVectors.Type()),
                 LinearAlgebra.FittingCategrory.Polynominal);
 
+            //TODO , calculate m_end1, m_en2
+
             base.OnValueChanged(sender, args);
+        }
+
+        /// <summary>
+        /// 1. Calculate projection onto line
+        /// 2. 
+        /// </summary>
+        internal void calculateEndPoints()
+        {
+            IEnumerable<MatExpr> errorList = m_dependencies.Select((ElementBase p) =>
+            {
+                return -1 * (m_coeff * (p as PointBase).Point);
+            });
+            //takes (a,b) only , should be 1x2
+            var subCoeff = m_coeff.SubMat(
+                0, 
+                m_coeff.Rows,
+                0, 
+                m_coeff.Cols - 1);
+
+            //map the vector perpendicular to line corresponding to each points
+            IEnumerable<Mat> subVectorList = errorList.Select((MatExpr y) =>
+            {
+                
+                return LinearAlgebra.DataFitting(subCoeff, y, LinearAlgebra.FittingCategrory.Polynominal);
+            });
+
+            IEnumerable<Mat> projectionList = subVectorList.Select((Mat sub) =>
+            {
+                int index = subVectorList.ToList().IndexOf(sub);
+                return (Mat)((m_dependencies[index] as PointBase).Point + sub);
+            });
+
+            //generate initial length table
+            var lengthTable = projectionList.Select((Mat point) =>
+            {
+                return ((Mat)(point - projectionList.First())).Norm();
+            });
+
+            //find the max one
+            int maxIndex = lengthTable.ToList().IndexOf(lengthTable.Max());
+            m_end1 = new PointBase(new List<ElementBase> { this});
+            m_end1.Point = projectionList.ToList()[maxIndex];
+
+            //find the another end
+            lengthTable = projectionList.Select((Mat point) =>
+            {
+                return ((Mat)(point - m_end1.Point)).Norm();
+            });
+            m_end2 = new PointBase(new List<ElementBase> { this });
+            m_end2.Point = projectionList.ToList()[lengthTable.ToList().IndexOf(lengthTable.Max())];
         }
     }
 }

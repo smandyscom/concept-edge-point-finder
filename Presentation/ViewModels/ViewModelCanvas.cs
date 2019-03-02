a6f45ada2d092f0797fa4ca94b60cb03dd5823ca
smandyscom <smandyscom@gmail.com>
2019-03-02 16:51:10 +0800

    added viewModel for canvas

---
+++ Presentation/ViewModels/ViewModelCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.ViewModels
+{
+    public class ViewModelCanvas
+    {
+        /// <summary>
+        /// The bag which manage all active objects
+        /// </summary>
+        public ObservableCollection<ViewModelBase> DrawObjects { get; set; } = 
+            new ObservableCollection<ViewModelBase>();
+    }
+}

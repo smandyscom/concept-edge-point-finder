32a00065aee0320f8679de0ece402b4c79b34d56
StevenKung <s790408p@gmail.com>
2019-03-03 15:41:09 +0800

    Create ElementBaseCollection and ViewModelCanvas

---
+++ ImageProfiler/ViewModels/ViewModelCanvas.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+using Core.Arch;
+using Core.Derived;
+using OpenCvSharp;
+using Presentation.ViewModels;
+
+namespace ImageProfiler.ViewModels
+{
+	public class ViewModelCanvas : INotifyPropertyChanged
+	{
+
+		/// <summary>
+		/// hold all ElementBase in bag
+		/// </summary>
+		public ElementBaseCollection Elements { get; set; } = new ElementBaseCollection();
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+
+
+	public class ElementBaseCollection : ObservableCollection<ViewModelBase>
+	{
+
+		public void AddRange(IList<ViewModelBase> list)
+		{
+			foreach (var item in list)
+				Add(item);
+		}
+
+		public void EnvelopElements(IList<ElementBase> list)
+		{
+			foreach (var item in list)
+			{
+				Add(ViewModelFactory.CreateViewModel(item));
+			}
+		}
+
+		public void ForEach(Action<ViewModelBase> action)
+		{
+			foreach (var item in this)
+			{
+				action(item);
+			}
+		}
+
+		#region TestonView
+
+		PointBase p2;
+		public ElementBaseCollection()
+		{
+			CoordinateBase c1 = new CoordinateBase();
+			PointBase p1 = new PointBase(new System.Collections.Generic.List<ElementBase> { c1 });
+			p2 = new PointBase(new System.Collections.Generic.List<ElementBase> { c1 });
+
+			p1.Point = new Mat(3, 1, MatType.CV_64FC1, new double[] { 0, 0, 1 });
+			p2.Point = new Mat(3, 1, MatType.CV_64FC1, new double[] { 512, 480, 1 });
+			LineBase line = new LineBase(new List<ElementBase>() { p1, p2 });
+
+			GrayImage img = new GrayImage(new List<ElementBase>() { c1 });
+			Cv2.CvtColor(Cv2.ImRead(@"..\..\lenna.png"), img.Image, ColorConversionCodes.BGR2GRAY);
+
+			PointEdge pointEdge = new PointEdge(new List<ElementBase> { img, line });
+
+
+			EnvelopElements(new List<ElementBase>() { c1, p1, p2, line, pointEdge });
+			CreateTimer();
+		}
+		void CreateTimer()
+		{
+			var timer1 = new Timer
+			{
+				Enabled = true,
+				Interval = 2000
+			};
+			timer1.Elapsed += Timer1_Elapsed;
+		}
+
+		private void Timer1_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			var mat = new Mat(3, 1, MatType.CV_64FC1, new double[] { -10, -10, 0 });
+			p2.Point += mat;
+		}
+
+		#endregion
+
+
+
+	}
+
+
+
+
+}
--- ImageProfiler/Views/UserControlCanvs.xaml.cs
+++ ImageProfiler/Views/UserControlCanvs.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Timers;
-using System.Windows;
+﻿using ImageProfiler.ViewModels;
 using System.Windows.Controls;
-using Core.Arch;
-using Core.Derived;
-using OpenCvSharp;
-using Presentation.ViewModels;
+
 
 
 namespace ImageProfiler
@@ -18,16 +10,14 @@ namespace ImageProfiler
 	/// </summary>
 	public partial class UserControlCanvs : UserControl
 	{
-		private ElementBaseCollection lines = new ElementBaseCollection();
-		//private Circles circles = new Circles();
-
+		private ViewModelCanvas elements = new ViewModelCanvas();
 		public UserControlCanvs()
 		{
 
 			InitializeComponent();
+			this.DataContext = elements;
 
 
-			this.DataContext = lines;
 			Border.MouseMove += Border_MouseMove;
 		}
 
@@ -42,51 +32,6 @@ namespace ImageProfiler
 		{
 			//Line obj = (Line)sender;
 			//var data = obj.DataContext;
-		}
-	}
-
-	public class ElementBaseCollection : ObservableCollection<ViewModelBase>
-	{
-
-		PointBase p2;
-		public ElementBaseCollection()
-		{
-			CoordinateBase c1 = new CoordinateBase();
-			PointBase p1 = new PointBase(new System.Collections.Generic.List<ElementBase> { c1 });
-			p2 = new PointBase(new System.Collections.Generic.List<ElementBase> { c1 });
-
-			p1.Point = new Mat(3, 1, MatType.CV_64FC1, new double[] { 0, 0, 1 });
-			p2.Point = new Mat(3, 1, MatType.CV_64FC1, new double[] { 512, 480, 1 });
-			LineBase line = new LineBase(new List<ElementBase>() { p1, p2 });
-
-			GrayImage img = new GrayImage(new List<ElementBase>() { c1 });
-			Cv2.CvtColor(Cv2.ImRead(@"..\..\lenna.png"), img.Image, ColorConversionCodes.BGR2GRAY);
-
-			PointEdge pointEdge = new PointEdge(new List<ElementBase> { img, line });
-
-
-
-			Add(ViewModelFactory.CreateViewModel(c1));
-			Add(ViewModelFactory.CreateViewModel(p1));
-			Add(ViewModelFactory.CreateViewModel(p2));
-			Add(ViewModelFactory.CreateViewModel(line));
-			Add(ViewModelFactory.CreateViewModel(pointEdge));
-			CreateTimer();
-		}
-		void CreateTimer()
-		{
-			var timer1 = new Timer
-			{
-				Enabled = true,
-				Interval = 2000
-			};
-			timer1.Elapsed += Timer1_Elapsed;
-		}
-
-		private void Timer1_Elapsed(object sender, ElapsedEventArgs e)
-		{
-			var mat = new Mat(3, 1, MatType.CV_64FC1, new double[] { -10, -10, 0 });
-			p2.Point +=mat;
 		}
 	}
 
